dfe6640022472bc51d1ee965332599467bbea5b8
CaseyKelly <casey.kelly@colorado.edu>
2015-03-29 17:24:45 -0600

    update bootstrap-sass

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,8 +12,8 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require bootstrap-sprockets
 //= require jquery-tablesorter
+//= require bootstrap-sprockets
 //= require turbolinks
 //= require_tree .
 
